0dab02dffc690e539e4517fd24090538e6d1b59c
shmartin1 <shmartin@crimson.ua.edu>
2016-11-08 16:14:25 -0600

    Added Beginnings of Member Class

---
+++ src/cs200fall2016team9/Member.java
@@ -0,0 +1,22 @@
+package cs200fall2016team9;
+
+public class Member extends DatabaseEntry {
+	
+	private String memberStatus;
+	
+	void Member(){
+	}
+	
+	void setStatus(String newStatus){
+		memberStatus=newStatus;
+	}
+	
+	String getStatus(){
+		return memberStatus;
+	}
+	
+	void addNewMember(){
+		System.out.println("Adding User...");
+		//Need to look up file stream util for Java
+	}
+}
