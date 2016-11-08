c16ce4df64fc06805e84f192102b29c58eb4afc0
jbmizzell1 <jakemizzell@gmail.com>
2016-11-08 12:11:00 -0600

    added ChocAn.class

---
+++ src/cs200fall2016team9/ChocAn.java
@@ -0,0 +1,28 @@
+package cs200fall2016team9;
+
+import java.util.Scanner; 
+
+public class ChocAn {
+    public static void main(String[] args) {
+        roleChoose();
+    }
+
+    private static void roleChoose() {
+        System.out.println("Do you want Provider, Manager, or Operator Role?");
+        Scanner scan = new Scanner(System.in);
+        String role = scan.next();
+        if ("Provider".equals(role))
+            System.out.println("You chose Provider");
+        else if ("Manager".equals(role))
+            System.out.println("You chose Manager");
+        else if ("Operator".equals(role))
+            System.out.println("You chose Operator");
+        else if ("Close".equals(role))
+            return;
+        else {
+            System.out.println("Error: must enter 'Provider', 'Manager', or 'Operator'");
+            roleChoose();
+        }
+        scan.close();
+    }
+}
