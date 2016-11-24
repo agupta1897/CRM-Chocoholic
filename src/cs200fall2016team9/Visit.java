d819dabbda68ecde39a5378bb626f7b6f579807e
amber <agupta18>
2016-11-23 20:02:08 -0600

    Added Visit and VisitDatabase

--- src/cs200fall2016team9/Visit.java
+++ src/cs200fall2016team9/Visit.java
@@ -1,11 +1,74 @@
 package cs200fall2016team9;
-
+import java.io.*;
 /**
  * 
  * @author Amber Gupta
  *
  */
 
-public class Visit {
+public abstract class Visit {
+	
+	private String currentDateAndTime;
+	private String date;
+	private int providerId;
+	private int memberId;
+	private int serviceCode;
+	private String comment;
 
+	abstract void addEntry() 
+
+	public static void submitVisitInfo(int providerId, int memberId, String currentDateAndTime, String date, int serviceCode, String comment) {
+		//abstract void saveVisit() throws IOException;
+		
+		
+		protected String getCurrentDateAndTime(){
+			return currentDateAndTime;
+		}
+		
+		protected String getDate(){
+			return date;
+		}
+		
+		protected String getComment(){
+			return comment;
+		}
+		
+		protected int getProviderId(){
+			return providerId;
+		}
+		protected int getMemberId(){
+			return memberId;
+		}
+		
+		protected int getServiceCode(){
+			return serviceCode;
+		}
+		
+		protected void setCurrentDateAndTime(String newcurrentDateAndTime){
+			currentDateAndTime= newcurrentDateAndTime;
+		}
+		
+		protected void setDate(String newDate){
+			date=newDate;
+		}
+		
+		protected void setComment(String newComment){
+			comment=newComment;
+		}
+		
+		protected void setProviderId(int newProviderId){
+			providerId=newProviderId;
+		}
+		
+		protected void setMemberId(int newMemberId){
+			memberId=newMemberId;
+		}
+		
+		protected void setServiceCode(int newServiceCode){
+			serviceCode =newServiceCode;
+		}
+	}
+
+	
+	}
 }
--- src/cs200fall2016team9/VisitDatabase.java
+++ src/cs200fall2016team9/VisitDatabase.java
@@ -1,11 +1,22 @@
 package cs200fall2016team9;
 
+import java.io.IOException;
+
 /**
  * 
  * @author Amber Gupta
  *
  */
 
-public class VisitDatabase {
+public class VisitDatabase extends Visit {
+
+	@Override
+	void addEntry() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 
 }
