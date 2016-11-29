6e97fa10dc951b01113d305d47150a7f9776a974
Mjbailey2 <Mjbailey2@crimson.ua.edu>
2016-11-29 13:01:51 -0600

    Did some stuff.

--- src/cs200fall2016team9/test/ProviderReportTest.java
+++ src/cs200fall2016team9/test/ProviderReportTest.java
@@ -2,6 +2,11 @@ package cs200fall2016team9.test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,11 +18,19 @@ public class ProviderReportTest {
 
 	@Before
 	public void setUp() throws Exception {
+		pr = new ProviderReport();
 	}
 
 	@Test
 	public void testProviderReportIntForSuccess() {
-		ProviderReport.providerReport(234567899);
+		try {
+			pr.providerReport(234567890);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String date= new SimpleDateFormat("MM-dd-yyyy").format(Calendar.getInstance().getTime());
+		File f = new File("src/files/report files/provider reports files/23456789011-29-2016Report.txt");
+		assertTrue(f.exists());
 	}
-
 }
